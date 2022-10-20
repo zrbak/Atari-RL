835ca96b71f8b0070437cb90172cad222a036220
ZachBaker2746 <zrbaker1554@gmail.com>
2022-10-19 21:13:15 -0400

    Added LunarLanderDemo.py

---
+++ LunarLanderDemo.py
@@ -0,0 +1,15 @@
+import gym
+
+env = gym.make("LunarLander-v2", render_mode="human")
+observation, info = env.reset(seed=42)
+
+# for _ in range(1000):
+   # action = policy(observation)  # User-defined policy function
+   # observation, reward, terminated, truncated, info = env.step(action)
+
+for _ in range(1000):
+   observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+   if terminated or truncated:
+      observation, info = env.reset()
+env.close()
+
